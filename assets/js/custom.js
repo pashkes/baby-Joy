f55230ba75b41d1590801c6db64ce1432832db6c
pashkes <pashkes@gmail.com>
2017-05-25 03:03:32 +0300

    Update 2017-05-25T00:03:30.719Z

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -7,6 +7,56 @@
 $(function () {
   svg4everybody();
 
+  //tabs
+  $(".js-tab-item").not(":first").hide();
+$(".js-tab").click(function() {
+    $(".js-tab").removeClass("single-product__tab-title--active")
+        .eq($(this).index()).addClass("single-product__tab-title--active");
+    $(".js-tab-item").hide().eq($(this).index()).show()
+}).eq(0).addClass("single-product__tab-title--active");
+
+  // Up and down number on input page catalog-item
+  if ($('.single-product__quantity').length) {
+    var $controlledByButtons = $('#controlledByButtons');
+    $controlledByButtons.updown({
+      step: 1,
+      shiftStep: 100,
+      min: 0
+    });
+    var $updown = $controlledByButtons.data('updown');
+    $('#btnDecrease').click(function(event){
+      $updown.increase(event);
+      $updown.triggerEvents();
+    });
+    $('#btnIncrease').click(function(event){
+      $updown.decrease(event);
+      $updown.triggerEvents();
+    });
+  }
+
+
+  //custom select
+  if($('.testselect1').length) {
+    $('.testselect1').SumoSelect();
+  }
+  //Slider Slider product page
+  if ($('.single-product').length) {
+    $('.js-single-product-big').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      asNavFor: '.js-product-single-nav'
+    });
+    $('.js-product-single-nav').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      centerPadding: 0,
+      asNavFor: '.js-single-product-big',
+      centerMode: true,
+      focusOnSelect: true
+    });
+  }
+
   //sorting product
   if ($('.js-sort').length) {
     $('.js-sort').on('click', function () {
@@ -22,10 +72,10 @@ $(function () {
     console.log(thisElementText);
 
     if ($(this).prop('checked')) {
-      $('.tag').append('<li class="tag__item"><span></span><button class="tag__remove" type="button">remove tag</button></li>').index();
+      $('.tag').append('<li class="tag__item"><span></span><button class="tag__remove" type="button">remove tag</button></li>').index(this);
       $('.tag__item span').text(thisElementText);
     } else{
-
+      $(this)
     }
   });
 
@@ -138,5 +188,4 @@ $(function () {
       });
     }
   });
-})
-;
+});
